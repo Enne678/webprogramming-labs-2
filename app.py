9799c7fa6f0a9d87bef3caadef8537968754e9d2
Алексей Миллер <miller.alex2004@gmail.com>
2024-09-26 11:34:15 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Миллер Алексей Евгеньевич, лабораторная 1</title>
+     </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </heder>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Алексей Миллер, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
