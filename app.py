caed81f20c03bde285a70ea3a2e3262ec15e288d
Алексей Миллер <miller.alex2004@gmail.com>
2024-09-26 12:51:03 +0700

    Меню

--- app.py
+++ app.py
@@ -2,23 +2,50 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+@app.route("/index")
+def index():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+        <nav>
+            <ul>
+                <li><a href="/lab1">Первая лабораторная</a></li>
+            </ul>
+        </nav>
+        <footer>
+            © Алексей Миллер, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
     <head>
         <title>Миллер Алексей Евгеньевич, лабораторная 1</title>
-     </head>
+    </head>
     <body>
         <header>
             НГТУ, ФБ, Лабораторная работа 1
-        </heder>
-
+        </header>
         <h1>web-сервер на flask</h1>
-
+        <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</p>
         <footer>
-            &copy; Алексей Миллер, ФБИ-24, 3 курс, 2024
+            © Алексей Миллер, ФБИ-24, 3 курс, 2024
         </footer>
     </body>
 </html>
 """
+
+if __name__ == "__main__":
+    app.run(debug=True)
