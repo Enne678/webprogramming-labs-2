2f4216754198ee8783c5430faed68261dbbf5627
Алексей Миллер <miller.alex2004@gmail.com>
2024-10-24 12:23:05 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request
 from lab1 import lab1
 from lab2 import lab2
-app = Flask(__name__)
+from lab3 import lab3
 
+app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -13,9 +15,7 @@ def start():
 
 @app.route("/menu")
 def menu():
-    return """
-<!doctype html>
-<html>
+    return """<!doctype html><html>
     <head>
         <title>НГТУ, ФБ, Лабораторные работы</title>
     </head>
@@ -27,13 +27,13 @@ def menu():
             <ul>
                 <li><a href="/lab1/">Первая лабораторная</a></li>
                 <li><a href="/lab2/">Вторая лабораторная</a></li>
+                <li><a href="/lab3/">Третья лабораторная</a></li>
             </ul>
         </nav>
         <footer>
             &copy; Алексей Миллер, ФБИ-24, 3 курс, 2024
         </footer>
-    </body>
-</html>"""
+    </body></html>"""
 
 @app.errorhandler(404)
 def page_not_found(error):
