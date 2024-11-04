54f60d41a243671675dfc89a9da9f785a5307db8
Алексей Миллер <miller.alex2004@gmail.com>
2024-11-05 06:27:37 +0700

    Очистить настройки кук

--- lab3.py
+++ lab3.py
@@ -90,6 +90,16 @@ def settings():
     
     return render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size, font_family=font_family)
 
+@lab3.route('/lab3/clear_settings_cookies')
+def clear_settings_cookies():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_family')
+    resp.set_cookie('bg_color', '#ffffff')
+    return resp
+
 @lab3.route('/lab3/ticket', methods=['GET', 'POST'])
 def ticket():
     if request.method == 'POST':
